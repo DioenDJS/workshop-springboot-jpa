62e995cadc933e1a2a21777059653be5c6a7e6d0
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2023-01-02 21:19:19 -0300

    :art: refactor method findAll and create method findById in controller

--- src/main/java/com/dioendweb/dioendweb/resources/UserResource.java
+++ src/main/java/com/dioendweb/dioendweb/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.dioendweb.dioendweb.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dioendweb.dioendweb.entities.User;
+import com.dioendweb.dioendweb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	//criando um method pra ser o endpoint pra acessar os usuarios
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list =  service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
