5feceebc66d9b0b759cea2b44bb81f457f493923
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2023-01-03 23:25:15 -0300

    :sparkles: create CategoryRepository

---
+++ src/main/java/com/dioendweb/dioendweb/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.dioendweb.dioendweb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dioendweb.dioendweb.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
