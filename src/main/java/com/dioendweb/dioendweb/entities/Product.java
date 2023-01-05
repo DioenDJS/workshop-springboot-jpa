537da75994bc652a0ad3248560471f6f1ab82a74
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2023-01-04 22:09:28 -0300

    :sparkles: create init class Entity Product

---
+++ src/main/java/com/dioendweb/dioendweb/entities/Product.java
@@ -0,0 +1,10 @@
+package com.dioendweb.dioendweb.entities;
+
+public class Product {
+
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+}
