33ae6ceaa58603d56cd6df13661831366ba69361
Diovane Pacheti <diovane.pacheti@l2code.com.br>
2023-01-08 10:19:06 -0300

    :recycle: create records new OrderItem

--- src/main/java/com/dioendweb/dioendweb/config/TestConfig.java
+++ src/main/java/com/dioendweb/dioendweb/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.dioendweb.dioendweb.entities.Category;
 import com.dioendweb.dioendweb.entities.Order;
+import com.dioendweb.dioendweb.entities.OrderItem;
 import com.dioendweb.dioendweb.entities.Product;
 import com.dioendweb.dioendweb.entities.User;
 import com.dioendweb.dioendweb.entities.enums.OrderStatus;
 import com.dioendweb.dioendweb.repositories.CategoryRepository;
+import com.dioendweb.dioendweb.repositories.OrderItemRepository;
 import com.dioendweb.dioendweb.repositories.OrderRepository;
 import com.dioendweb.dioendweb.repositories.ProductRepository;
 import com.dioendweb.dioendweb.repositories.UserRepository;
@@ -34,6 +36,8 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -68,6 +72,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 
 }
